d07bf42a9a5820c5fa0a5d7d14c7c2f6ec9080d6
Dang Hai <pecheule34713@gmail.com>
2024-06-18 13:55:36 +0700

    initial-commit

---
+++ src/main/java/org/senju/eshopeule/EShopEuleApplication.java
@@ -0,0 +1,13 @@
+package org.senju.eshopeule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopEuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShopEuleApplication.class, args);
+	}
+
+}
