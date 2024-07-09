17b47f9f2c3d5e433f0c7b82e8ce9d4919447485
Dang Hai <pecheule34713@gmail.com>
2024-07-09 23:52:19 +0700

    update

--- src/main/java/org/senju/eshopeule/controller/AuthController.java
+++ src/main/java/org/senju/eshopeule/controller/AuthController.java
@@ -13,13 +13,8 @@ import org.senju.eshopeule.exceptions.*;
 import org.senju.eshopeule.service.AuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
 
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
